84a6e8996dacd052b98398a2a9aba2989f381ecb
Usuario <Usuario@CPD02-M16>
2024-03-04 12:20:41 -0300

    Avanço no Projeto

--- MauiAppListaCompras/App.xaml.cs
+++ MauiAppListaCompras/App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace MauiAppListaCompras
+﻿using MauiAppListaCompras.Helpers;
+namespace MauiAppListaCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper db
+        {
+            get
+            {
+                if(db == null)
+                {
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco_sqlite_compras.db3");
+
+                   _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
