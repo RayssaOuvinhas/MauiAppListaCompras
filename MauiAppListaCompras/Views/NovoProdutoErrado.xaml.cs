409042c5af65052e8d174e3876a76470e25833ca
Usuario <Usuario@CPD03-M06>
2024-03-11 12:23:35 -0300

    Novas mudanças

---
+++ MauiAppListaCompras/Views/NovoProdutoErrado.xaml.cs
@@ -0,0 +1,41 @@
+﻿using AndroidX.Core.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MauiAppListaCompras.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NovoProduto : ContentPage
+    {
+        public NovoProduto()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Produto p = new produto
+                {
+                    Descricao = txt_descricao.Text,
+                    Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                    Preco = Convert.ToDouble(txt_preco.Text),
+                };
+
+                await App.db.Update(p);
+                await DisplayAlert("Sucesso!", "Produto Editado!", "OK");
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "Ok");
+            }
+        }
+    }
+}
